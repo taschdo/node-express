2c9b97cf747620365426d0ffdcee9b74301affc4
taschdo <taschdo@gmail.com>
2020-01-20 17:39:08 +0300

    17:38 20/01/2020

--- index.js
+++ index.js
@@ -13,12 +13,27 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Главная страница',
+        isHome: true
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: 'Курсы',
+        isCourses: true
+    })
+})
+
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Добавить курс',
+        isAdd: true
+    })
 })
 
 const PORT = process.env.PORT || 3000
